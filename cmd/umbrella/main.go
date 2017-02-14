5d412c90cc10fd5a770d72479031e73812a03d34
Shintaro Kaneko <kaneshin0120@gmail.com>
2017-02-14 03:40:28 +0000

    Add abs function

--- cmd/umbrella/main.go
+++ cmd/umbrella/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+var (
+	dir = flag.String("path", "", "")
+)
+
+func abs(name string) (string, error) {
+	if name == "" {
+		return os.Getwd()
+	}
+
+	if !path.IsAbs(name) {
+		abs, err := filepath.Abs(name)
+		if err != nil {
+			return "", err
+		}
+		name = abs
+	}
+
+	_, err := os.Stat(name)
+	if err != nil {
+		return "", err
+	}
+	return name, nil
+}
+
+func init() {
+	flag.Parse()
+
+	abs, err := abs(*dir)
+	if err != nil {
+		log.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	*dir = abs
+}
 
 func main() {
-	fmt.Println("umbrella")
+	fmt.Println(*dir)
 }
---
+++ cmd/umbrella/main_test.go
@@ -0,0 +1,6 @@
+package main
+
+import "testing"
+
+func Test_abs(t *testing.T) {
+}
